2cc6774eaa13031ca02a149a98af8b1f31376fde
tmilne1 <tmilne1@gmail.com>
2019-12-06 22:31:52 -0500

    added JS to toggle classList on click and transition end

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("hello")
+window.onload = function(){
+
+const panels = document.querySelectorAll(".panel")
+
+function open(){this.classList.toggle('open')}
+function active(event){
+    if(event.propertyName.includes("flex")){
+        this.classList.toggle('open-active')
+    }
+}
+
+panels.forEach((panel)=>{  
+    panel.addEventListener('click', open)
+    panel.addEventListener('transitionend',active )
+})
+
+}
